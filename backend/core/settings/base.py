5411c1595a5e7b502769d1277baca35091259ff8
Kaushal Neupane <kaushal98211@gmail.com>
2026-03-31 00:36:19 +0545

    initial setup of user model with roles

--- backend/core/settings/base.py
+++ backend/core/settings/base.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.users',
+'apps.vehicles',
+'apps.bookings',
+'apps.reviews',
 ]
 
 MIDDLEWARE = [
@@ -79,6 +83,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = "users.User"
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
